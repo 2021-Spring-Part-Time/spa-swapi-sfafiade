bb538ba4663f6cbcf5fb61cbb805ef23da57eb75
sfafiade <thefaf17@gmail.com>
2021-07-10 16:48:24 -0400

    update

--- js/components/People.js
+++ js/components/People.js
@@ -1,25 +1,15 @@
+import PersonTile from './PersonTile';
+
 export default function People(people) {
-    return `
-    <div class='people__container'>
-    ${people.results
-      .map((person) => {
-        return `
-        <div class='person__tile_container'>
-            <h2>${person.name}</h2>
-            <p>Hair Color: ${person.hair_color}</p>
-            <p>Birth Year: ${person.birth_year}</p>
-            <h3>Films:</h3>
-            ${person.films
-              .map((film) => {
-                return `
-                    <p>${film}</p>
-                `;
-              })
-              .join('')}
-        </div>
-        `;
-      })
-      .join('')}
+  return `
+  <div class='people__container'>
+  ${people.results
+    .map((person) => {
+      return `
+      ${PersonTile(person)}
+      `;
+    })
+    .join('')}
 </div>
     `;
 }
---
+++ js/pages/PersonPage.js
@@ -0,0 +1,9 @@
+
+import PersonTile from '../components/PersonTile.js';
+
+export default function PersonPage(person) {
+  return `
+    <h1>The Person Page</h1>
+    ${PersonTile(person)}
+    `;
+}
