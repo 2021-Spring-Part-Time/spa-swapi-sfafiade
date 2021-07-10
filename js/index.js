cbcdc6446d9187b8536d4184e0f0e8d58bc5d407
sfafiade <thefaf17@gmail.com>
2021-07-10 11:55:16 -0400

    update 2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-import Header from './components/Header';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import HomePage from './pages/HomePage.js';
+import apiActions from './api-actions/api-actions';
 
 buildPage();
 
 function buildPage(){
     header();
+    footer();
+    navigateToHomePage();
 }
 
 function header() {
     const headerElement = document.querySelector('.header');
     headerElement.innerHTML = Header();
+}
+
+function footer() {
+    const headerElement = document.querySelector('.footer');
+    headerElement.innerHTML = Footer();
+}
+
+function navigateToHomePage() {
+    const homeButton = document.querySelector('.nav__list_home');
+    homeButton.addEventListener('click', ()=> {
+        const app = document.querySelector('#app');
+        app.innerHTML = HomePage();
+    });
+}
+
+function renderPeopleInfoList(){
+    const peopleButton = document.querySelector('.nav__list_people')
+    peopleButton.addEventListener('click', () => {
+        getRequest('https://swapi.dev/api/people', (people) => {
+            console.log(people);
+        });
+    });
 }
